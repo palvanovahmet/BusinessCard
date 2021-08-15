3e51784e329260e8434a51861115f1af98ed9917
github - palvanovahmet <palvanovahmet1@gmail.com>
2021-08-15 19:07:02 +0300

    about-page-components

---
+++ Irina/src/components/About.jsx
@@ -0,0 +1,150 @@
+function About() {
+    return (
+        <div className="background">
+            <div class="line">
+
+            </div>
+
+            <header>
+            <div class="wrapper">
+            <div class="header__container">
+                <div class="header__logo">
+                    <img src="assets/beats.png" width="40" height="40"/>
+                    <div class="header__line">
+
+                    </div>
+                    <span class="header__brand__name">Ivanov</span>
+                </div>
+
+                <div class="header__links">
+                    <nav class="menu">
+                        <ul class="menu__items">
+                            <li class="menu__list"><a href="#" class="links">Главная страница</a></li>
+                            <li class="menu__list"><a href="#" class="current__link">Обо мне</a></li>
+                            <li class="menu__list"><a href="#" class="links">Услуги</a></li>
+                            <li class="menu__list"><a href="#" class="links">Контакты</a></li>
+                        </ul>
+                    </nav>
+                     </div>
+                    </div>
+                    </div>
+                </header>
+
+        <main>
+            <div class="social__network">
+                <div class="social">
+                    <div class="phone">
+                        <span>Phone</span>
+                        <span>+79220768300</span>
+                    </div>
+
+                    <div class="email">
+                        <span>Email</span>
+                        <span>Pavlovaira18@yandex.ru</span>
+                    </div>
+
+                    <div class="networks">
+                        <span>Social Network</span>
+
+                    <div class="social__links">
+                        <ul>
+                            <li><a href="#"><img src="assets/Group.svg"/></a></li>
+                            <li><a href="#"><img src="assets/Group.svg"/></a></li>
+                            <li><a href="#"><img src="assets/Group.svg"/></a></li>
+                            <li><a href="#"><img src="assets/Group.svg"/></a></li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="main__container">
+            <h1>About</h1>
+            <p>I'm Irina Pavlova, web designer with 3 years of experience. I'm 24 years<br/>old, during my work in digital, I passed the stage from a beginner<br/>a senior designer.</p>
+            <p>I create turkney sites and design pages in a social networks. It is possible to<br/>create a site in Tilda, or layout of the program code depending on the<br/>task and the brief.</p>
+        </div>
+
+        <div class="soft__skills">
+            <div class="education">
+                <h1>educations</h1>
+                <div class="university">
+                    <span>Designer</span>
+                    <p>Tyumen state university<br/>of Architecture</p>
+                    <p>Session: 2014-2019</p>
+                </div>
+
+                <div class="another__education">
+                    <span>Web design</span>
+                    <p>Online university skillbox</p>
+                    <p>Session: 2018-2020</p>
+                </div>
+            </div>
+
+            <div class="skills">
+               
+                <h1>Skills</h1>
+
+                <div class="progress">
+                    <h4>photoshop</h4>
+                    <div class="progress__bar">
+                        <div class="photoshop">
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="progress">
+                    <h4>Figma</h4>
+                    <div class="progress__bar">
+                        <div class="figma">
+
+                        </div>
+                    </div>
+                </div>
+                    
+                <div class="progress">
+                    <h4>illustrator</h4>
+                    <div class="progress__bar">
+                        <div class="illustrator">
+
+                        </div>
+                    </div>
+                </div>
+                   
+                <div class="progress">
+                    <h4>Tilda</h4>
+                    <div class="progress__bar">
+                        <div class="tilda">
+
+                        </div>
+                    </div>
+                </div>
+                   
+                <div class="progress">
+                    <h4>Html</h4>
+                    <div class="progress__bar">
+                        <div class="html">
+
+                        </div>
+                    </div>
+                </div>
+                   
+                <div class="progress">
+                    <h4>Css</h4>
+                    <div class="progress__bar">
+                        <div class="css">
+
+                        </div>
+                    </div>
+                </div>     
+            </div>
+        </div>
+    </main>
+                
+    </div>
+    );
+}
+
+export default About;
---
+++ Irina/src/components/Test.jsx
@@ -0,0 +1,9 @@
+function Test() {
+    return (
+        <div>
+            <p>Hello world</p>
+        </div>
+    );
+}
+
+export default Test;
